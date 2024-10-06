b73d42fbe09d80d4426edb2694d13445623e136a
arastepa <stepanyan2298@gmail.com>
2024-10-07 00:51:27 +0400

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [`${import.meta.dirname}/files/script.js`, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe']
+    });
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.pipe(process.stdout);
+
+    child.stderr.on('data', (data) => {
+        process.stderr.write(`Error from child: ${data}`);
+    });
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["werewolves", "vampires"]);
